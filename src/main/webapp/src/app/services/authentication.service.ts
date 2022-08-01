9846472c83fbabece3f637d3515e8afde2fc5169
mohkelmohamady <moh.k.elmohamady@gmail.com>
2022-08-01 21:33:06 +0200

    Added authentication service (maybe subject to future change) and added user interface

---
+++ src/main/webapp/src/app/services/authentication.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
+import { Observable } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class AuthenticationService {
+    constructor(private httpClient: HttpClient) { }
+    
+    private api = "http://localhost:8080/user";
+
+    public login(user : User) : Observable<User> {
+        return this.httpClient.post<User>(this.api + "/logout", user);
+    }
+
+    public logout() : Observable<User> {
+        return this.httpClient.post<User>(this.api + "/logout", {});
+    }
+    
+}
