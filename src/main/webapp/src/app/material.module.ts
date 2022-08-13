fb45df9e69f0d65fa41e2ea8d78ad5dd92bf0dcd
mohkelmohamady <moh.k.elmohamady@gmail.com>
2022-08-13 10:00:12 +0200

    Added material module to import all required modules from Angular material (so far)

--- src/main/webapp/src/app/material.module.ts
+++ src/main/webapp/src/app/material.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatGridListModule} from '@angular/material/grid-list'
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatSnackBarModule} from '@angular/material/snack-bar'
 const materialComponents = [
-  MatGridListModule
+  MatGridListModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatTooltipModule,
+  MatSnackBarModule,
 ]
 
 
