cec00034776f87ab14106827a663c8d3a11fcb35
mohkelmohamady <moh.k.elmohamady@gmail.com>
2022-08-13 13:39:36 +0200

    Implemented the Assignment constructor that takes a builder to set its fields (was missing previously)

--- src/main/java/io/horatius/farouk_university/models/Assessment.java
+++ src/main/java/io/horatius/farouk_university/models/Assessment.java
@@ -1,6 +1,6 @@
 package io.horatius.farouk_university.models;
 
-import io.horatius.farouk_university.dao.Scholar;
+import io.horatius.farouk_university.dao.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +13,7 @@ import java.util.UUID;
 public class Assessment {
     private UUID assessmentId;
     private Submission submission;
-    private Scholar scholar;
+    private User scholar;
     private int grade;
     private String comment;
 
