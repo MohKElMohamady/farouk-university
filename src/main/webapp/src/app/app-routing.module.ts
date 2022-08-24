ddf5d972597cd054fec45159938ccd0e3e92bd14
mohkelmohamady <moh.k.elmohamady@gmail.com>
2022-08-25 00:00:05 +0200

    Added the app routing module

--- src/main/webapp/src/app/app-routing.module.ts
+++ src/main/webapp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardContainerComponent } from './courses/dashboard/dashboard-container/dashboard-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "dashboard", component : DashboardContainerComponent},
+  {path : "", redirectTo : "dashboard", pathMatch : "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
