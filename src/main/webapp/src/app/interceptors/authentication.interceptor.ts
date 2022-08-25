483661436b62e0e12f517df06a68ffc44afcf71e
mohkelmohamady <moh.k.elmohamady@gmail.com>
2022-08-25 23:32:09 +0200

    Added authentication component for logging in

--- src/main/webapp/src/app/interceptors/authentication.interceptor.ts
+++ src/main/webapp/src/app/interceptors/authentication.interceptor.ts
@@ -18,13 +18,12 @@ export class BasicAuthHeader implements HttpInterceptor {
     /*
      * Placeholder to retrieve username and password from local storage after implementing login
      */
-
     const username = localStorage.getItem("username");
     const password = localStorage.getItem("password");
     const authReq = req.clone({
       setHeaders: {
         'Content-Type': 'application/json',
-        Authorization: 'Basic ' + btoa('username:password'),
+        Authorization: 'Basic ' + btoa('steve.eisman:MistookLeverageForGenius'),
       },
     });
     return next.handle(authReq);
--- src/main/webapp/src/app/user/user.module.ts
+++ src/main/webapp/src/app/user/user.module.ts
@@ -4,14 +4,30 @@ import { EffectsModule } from '@ngrx/effects';
 import { userReducers } from './state/user.reducers';
 import { StoreModule } from '@ngrx/store';
 import {UserEffects} from './state/user.effects';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { MaterialModule } from '../material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegistrationComponent } from './registration/registration.component';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthenticationComponent,
+    RegistrationComponent
+  ],
   imports: [
     CommonModule,
+    SharedModule,
+    MaterialModule,
+    ReactiveFormsModule,
     EffectsModule.forFeature([UserEffects]),
-    StoreModule.forFeature("User", userReducers)
+    StoreModule.forFeature("User", userReducers),
+    RouterModule.forChild([
+      {path : "login", component : AuthenticationComponent},
+      {path: "register", component : RegistrationComponent}
+    ])
   ]
 })
 export class UserModule { }
